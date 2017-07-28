cd378110fb3b83996e66b3dd721963647aa19577
Luis G. Valle <luis.gvalle@gmail.com>
2017-07-28 16:13:58 +0100

    Clean up cloud function

--- functions/index.js
+++ functions/index.js
@@ -8,64 +8,63 @@ admin.initializeApp(functions.config().firebase);
 
 exports.fetch = functions.https.onRequest((req, res) => {
     console.log("function starting");
-
-    var lastEdition = admin.database().ref('/manufacturer/sony/messages/last');
+    var lastEdition = admin.database().ref('/feed/last');
     lastEdition.once("value")
-        .then(function (snapshot) {
-            var exists = (snapshot.val() !== null);
-
-            if (exists) {
-                console.log("Exist -> return from DB")
-                response(res, snapshot.val())
-                
-            } else {
-                console.log("Missing -> fetch")
-                // Grab the current value of what was written to the Realtime Database.
-                client.get("https://ep00.epimg.net/rss/elpais/portada.xml", function (data, response) {
-                    // parsed response body as js object 
-                    console.log("feed fetched");
-                    // raw response 
-                    //console.log(response);
-
-
-                    const channel = data.rss.channel
-                    const items = []
-
-                    channel.item.forEach(element => {
-                        const item = {
-                            title: element.title,
-                            link: element.link,
-                            date: element.pubDate,
-                            description: element.description,
-                            content: element["content:encoded"] || null, // prevent undefined insert into Firebase DB
-                            creator: element["dc:creator"],
-                            media: []
-                        }
-
-                        element.enclosure.forEach(enclosure => {
-                            item.media.push({
-                                url: enclosure.$.url,
-                                type: enclosure.$.type
-                            })
-                        });
-
-                        items.push(item);
-                        console.log("item parsed")
-                    });
-
-                    lastEdition.push().set(items).then(snapshot => {
-                        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-                        console.log("items pushed to firebase");
-                    });
-
-                    response(res, items);
-                });
-            }
-        });
+        .then(snapshot => parse(snapshot, res, lastEdition));
 });
 
-function response(res, items) {
-    res.status(200)
+function parse(snapshot, res, lastEdition) {
+    var exists = (snapshot.val() !== null);
+
+    if (exists) {
+        console.log("Exist -> return from DB")
+        response(res, 200, snapshot.val());
+
+    } else {
+        console.log("Missing -> fetch")
+        client.get("https://ep00.epimg.net/rss/elpais/portada.xml", function (data, response) {
+            console.log("feed fetched");
+            const items = parseChannel(data.rss.channel)
+
+            lastEdition.push().set(items)
+                .then(() => console.log("items pushed to firebase"))
+                .then(() => response(res, 201, items));
+        });
+    }
+}
+
+function response(res, code, elements) {
+    return res.status(code)
         .type('application/json')
-        .send(items);
+        .send(elements);
+}
+
+function parseChannel(channel) {
+    const items = []
+
+    channel.item.forEach(element => {
+        const item = makeItem(element);
+        element.enclosure.forEach(enclosure => {
+            item.media.push({
+                url: enclosure.$.url,
+                type: enclosure.$.type
+            })
+        });
+
+        items.push(item);
+        console.log("item parsed")
+    });
+    return items;
+}
+
+function makeItem(element) {
+    return {
+        title: element.title,
+        link: element.link,
+        date: element.pubDate,
+        description: element.description,
+        content: element["content:encoded"] || null, // prevent undefined insert into Firebase DB
+        creator: element["dc:creator"],
+        media: []
+    }
 }
